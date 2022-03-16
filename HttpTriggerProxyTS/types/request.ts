a0bf46900fff250c958382cee6a44c055a752226
Aidan Timson <contact@timmo.xyz>
2022-03-16 16:13:59 +0000

    Reorder

--- HttpTriggerProxyTS/types/request.ts
+++ HttpTriggerProxyTS/types/request.ts
@@ -1,11 +1,11 @@
 import { ServiceBusMessage } from "@azure/service-bus";
 
-export interface ReceiveRequestQuery {
-  count: number;
-  queueName: string;
-}
-
 export interface QueueRequestBody {
   queueName: string;
   messages: Array<ServiceBusMessage>;
+}
+
+export interface ReceiveRequestQuery {
+  count: number;
+  queueName: string;
 }
